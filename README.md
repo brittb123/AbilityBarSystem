94743d00af9957a3f1d3597705a05e29b82c2d37
Britt <brittboudreaux@yahoo.com>
2022-09-27 17:32:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AbilityBarSwapper
+
+Developed with Unreal Engine 4
---
+++ Source/AbilityBarSwapper/AbilityBarSwapperGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AbilityBarSwapperGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABILITYBARSWAPPER_API AAbilityBarSwapperGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
